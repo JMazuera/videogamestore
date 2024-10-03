24d2f3cb69f6a545c2fd46a66fcc0a5b9d217f54
Juan Mazuera <j.mazuera75@gmail.com>
2024-10-02 23:55:34 -0500

    feature enhancement: added OpenAPI documentation and annotations to VideoGameController

--- src/main/java/co/com/devco/talentpool/videogamestore/infrastructure/controllers/CustomerController.java
+++ src/main/java/co/com/devco/talentpool/videogamestore/infrastructure/controllers/CustomerController.java
@@ -2,6 +2,12 @@ package co.com.devco.talentpool.videogamestore.infrastructure.controllers;
 
 import co.com.devco.talentpool.videogamestore.application.services.CustomerService;
 import co.com.devco.talentpool.videogamestore.domain.models.Customer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,37 +24,74 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @Operation(summary = "Create a new customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Customer created",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
         Customer createdCustomer = customerService.createCustomer(customer);
         return new ResponseEntity<>(createdCustomer, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get a customer by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the customer",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class))),
+            @ApiResponse(responseCode = "404", description = "Customer not found", content = @Content)
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id){
+    public ResponseEntity<Customer> getCustomerById(@Parameter(description = "id of customer to be searched")
+                                                    @PathVariable Long id) {
         return customerService.getCustomer(id)
                 .map(customer -> new ResponseEntity<>(customer, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Get all customers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of customers",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class)))
+    })
     @GetMapping
-    public ResponseEntity<List<Customer>> getAllCustomers(){
+    public ResponseEntity<List<Customer>> getAllCustomers() {
         List<Customer> customerList = customerService.getAllCustomers();
         return new ResponseEntity<>(customerList, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update an existing customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Customer updated",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class))),
+            @ApiResponse(responseCode = "404", description = "Customer not found", content = @Content)
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer){
+    public ResponseEntity<Customer> updateCustomer(@Parameter(description = "id of customer to be updated")
+                                                   @PathVariable Long id,
+                                                   @RequestBody Customer customer) {
         return customerService.updateCustomer(id, customer)
-                .map(customer1 -> new ResponseEntity<>(customer, HttpStatus.OK))
+                .map(updatedCustomer -> new ResponseEntity<>(updatedCustomer, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Delete a customer by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Customer deleted", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Customer not found", content = @Content)
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id){
-        if(customerService.deleteCustomer(id)){
+    public ResponseEntity<Void> deleteCustomer(@Parameter(description = "id of customer to be deleted")
+                                               @PathVariable Long id) {
+        if (customerService.deleteCustomer(id)) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
+
--- src/main/java/co/com/devco/talentpool/videogamestore/infrastructure/controllers/VideoGameController.java
+++ src/main/java/co/com/devco/talentpool/videogamestore/infrastructure/controllers/VideoGameController.java
@@ -3,6 +3,12 @@ package co.com.devco.talentpool.videogamestore.infrastructure.controllers;
 import co.com.devco.talentpool.videogamestore.application.services.VideoGameService;
 import co.com.devco.talentpool.videogamestore.domain.exceptions.NoCopiesAvailableException;
 import co.com.devco.talentpool.videogamestore.domain.models.VideoGame;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,42 +25,86 @@ public class VideoGameController {
         this.videoGameService = videoGameService;
     }
 
+    @Operation(summary = "Create a new video game")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Video game created",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VideoGame.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content)
+    })
     @PostMapping
-    public ResponseEntity<VideoGame> createVideoGame(@RequestBody VideoGame videoGame){
+    public ResponseEntity<VideoGame> createVideoGame(@RequestBody VideoGame videoGame) {
         VideoGame createdVideoGame = videoGameService.createVideoGame(videoGame);
         return new ResponseEntity<>(createdVideoGame, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get a video game by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the video game",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VideoGame.class))),
+            @ApiResponse(responseCode = "404", description = "Video game not found", content = @Content)
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<VideoGame> getVideoGameById(@PathVariable Long id){
+    public ResponseEntity<VideoGame> getVideoGameById(@Parameter(description = "id of video game to be searched")
+                                                      @PathVariable Long id) {
         return videoGameService.getVideoGame(id)
                 .map(videoGame -> new ResponseEntity<>(videoGame, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Get all video games")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of video games",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VideoGame.class)))
+    })
     @GetMapping
-    public ResponseEntity<List<VideoGame>> getAllVideoGames(){
+    public ResponseEntity<List<VideoGame>> getAllVideoGames() {
         List<VideoGame> videoGameList = videoGameService.getAllVideoGames();
         return new ResponseEntity<>(videoGameList, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update an existing video game")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Video game updated",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VideoGame.class))),
+            @ApiResponse(responseCode = "404", description = "Video game not found", content = @Content)
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<VideoGame> updateVideoGame(@PathVariable Long id, @RequestBody VideoGame updateVideoGame){
+    public ResponseEntity<VideoGame> updateVideoGame(@Parameter(description = "id of video game to be updated")
+                                                     @PathVariable Long id,
+                                                     @RequestBody VideoGame updateVideoGame) {
         return videoGameService.updateVideoGame(id, updateVideoGame)
                 .map(videoGame -> new ResponseEntity<>(videoGame, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Delete a video game by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Video game deleted", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Video game not found", content = @Content)
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteVideoGame(@PathVariable Long id){
+    public ResponseEntity<Void> deleteVideoGame(@Parameter(description = "id of video game to be deleted")
+                                                @PathVariable Long id) {
         if (videoGameService.deleteVideoGame(id)) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Purchase a video game by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Video game purchased successfully",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "No copies available", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error processing the purchase", content = @Content)
+    })
     @PostMapping("/{id}/purchase")
-    public ResponseEntity<String> purchaseVideoGame(@PathVariable Long id) {
+    public ResponseEntity<String> purchaseVideoGame(@Parameter(description = "id of video game to be purchased")
+                                                    @PathVariable Long id) {
         try {
             videoGameService.purchaseVideoGame(id);
             return new ResponseEntity<>("Video game purchased successfully!", HttpStatus.OK);
@@ -65,3 +115,4 @@ public class VideoGameController {
         }
     }
 }
+
